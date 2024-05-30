48ff4027b3ea24c748561b589c032adcbf6bd239
hardik2302 <64209422+hardik2302@users.noreply.github.com>
2024-05-30 13:56:51 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull hardikagrawal2320/calculator
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 hardikagrawal2320/calculator
